4f9dd3cd19a21e5d9178bc3176c4d7911aacc11d
Christopher Sayon <Christopher.L.Sayon@gmail.com>
2020-09-27 19:34:40 +1000

    Added winner functionality

--- app.js
+++ app.js
@@ -40,25 +40,46 @@ document.querySelector('.btn-roll').addEventListener('click', function btn() {
         document.querySelector('#current-' + activePlayer).textContent = roundScore;
     } else {
         //Next player
-        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
-        roundScore = 0;
-
-        document.getElementById('current-0').textContent = '0';
-        document.getElementById('current-1').textContent = '0';
-
-        //Toggle active panel player
-        document.querySelector('.player-0-panel').classList.toggle('active');
-        document.querySelector('.player-1-panel').classList.toggle('active');
-
-        // document.querySelector('.player-0-panel').classList.remove('active');
-        // document.querySelector('.player-1-panel').classList.add('active');
-
-        document.querySelector('.dice').style.display = 'none';
+        nextPlayer();
     }
 });
 
+document.querySelector('.btn-hold').addEventListener('click', function() {
+    //Add current score to player's global score
+    scores[activePlayer] += roundScore;
 
+    //Update the UI
+    document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
 
+    //Check if player won the game
+    if (scores[activePlayer] >= 6) {
+        document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
+        document.querySelector('.dice').style.display = 'none';
+        document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner')
+        document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active')
+    } else {
+        //Next player
+        nextPlayer();
+    }
+});
+
+function nextPlayer() {
+    //Next player
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    roundScore = 0;
+
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+
+    //Toggle active panel player
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+
+    // document.querySelector('.player-0-panel').classList.remove('active');
+    // document.querySelector('.player-1-panel').classList.add('active');
+
+    document.querySelector('.dice').style.display = 'none';
+}
 
 // Selects the first element that it finds
 // document.querySelector('#current-' + activePlayer).textContent = dice;
