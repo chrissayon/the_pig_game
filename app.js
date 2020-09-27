85cf52ecb6db72564361a8ea209b63435471c677
Christopher Sayon <Christopher.L.Sayon@gmail.com>
2020-09-27 18:31:50 +1000

    Changed dice display and set initial score to 0

--- app.js
+++ app.js
@@ -16,15 +16,34 @@ scores = [0, 0];
 roundScore = 0;
 activePlayer = 0;
 
-dice = Math.floor(Math.random() * 6) + 1;
-
-// Selects the first element that it finds
-document.querySelector('#current-' + activePlayer).textContent = dice;
-// document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
-
-//Obtain total score
-var x = document.querySelector('#score-0').textContent;
-console.log(x);
-
 //Hide dice at the beginning
 document.querySelector('.dice').style.display = 'none';
+
+document.getElementById('score-0').textContent = '0'
+document.getElementById('score-1').textContent = '0'
+document.getElementById('current-0').textContent = '0'
+document.getElementById('current-1').textContent = '0'
+
+document.querySelector('.btn-roll').addEventListener('click', function btn() {
+    // 1. Random Number
+    dice = Math.floor(Math.random() * 6) + 1;
+    
+    // 2. Display the result
+    var diceDOM = document.querySelector('.dice'); 
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-' + dice + '.png';
+
+    // 3. Update the round score if the tolled number was not a 1
+});
+
+
+
+
+// Selects the first element that it finds
+// document.querySelector('#current-' + activePlayer).textContent = dice;
+// document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
+
+
+// //Obtain total score
+// var x = document.querySelector('#score-0').textContent;
+// console.log(x);
